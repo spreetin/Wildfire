d1fc7690d4674fc999bead4abfae3b92d2880675
David Falk <dafa2100@student.miun.se>
2024-04-24 14:46:03 +0200

    Basic setup and timer for Controller.

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,10 +1,37 @@
-public class Controller {
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Controller implements ActionListener {
+
+    final View view = new View();
+    final Model model = new Model();
+    final Calculator calculator = new Calculator();
+    final FrameStatistics frameStatistics = new FrameStatistics();
+
+    Timer timer = new Timer(1000, this);
+
+    public Controller(){
+        model.registerListener(view);
+        model.registerListener(frameStatistics);
+        calculator.registerListener(model);
+    }
+
+    public void setTickSpeed(int msecs){
+        timer.setDelay(msecs);
+    }
 
     public void startTimer(){
-        // TODO: Implement
+        timer.start();
     }
 
     public void pauseTimer(){
+        timer.stop();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
         // TODO: Implement
+        // Requests a calculation of the next frame
     }
 }
