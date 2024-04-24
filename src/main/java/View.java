5a933adfc628b50e22c80ebc8d7714aeb2320da6
David Falk <dafa2100@student.miun.se>
2024-04-24 14:52:53 +0200

    Added the first few lines on View for getting an update.

--- src/main/java/View.java
+++ src/main/java/View.java
@@ -1,6 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class View implements Observer{
     @Override
     public void newUpdate(Object o) {
-
+        // Cast o to Model class
+        if (!(o instanceof Model model))
+            return;
+        Coordinate[] updatedCells = model.updatedCells();
+        // TODO: Implement
     }
 }
