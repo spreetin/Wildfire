cf39ada20e6321833ded9a672f92c97413971dd2
David Falk <dafa2100@student.miun.se>
2024-04-25 10:38:58 +0200

    Added tests for InitialMap.

--- src/main/java/se/oru/wildfire/InitialMap.java
+++ src/main/java/se/oru/wildfire/InitialMap.java
@@ -5,6 +5,12 @@ import se.oru.wildfire.Coordinate;
 
 public class InitialMap {
 
+    private Cell[][] map;
+
+    public InitialMap(Cell[][] mapData){
+        map = mapData;
+    }
+
     public Coordinate getSize(){
         // TODO: Implement
         return null;
@@ -13,5 +19,9 @@ public class InitialMap {
     public Cell getCell(int x, int y){
         // TODO: Implement
         return null;
+    }
+
+    public Cell getCell(Coordinate coordinate){
+        return getCell(coordinate.x(), coordinate.y());
     }
 }
--- src/test/java/InitialMapTest.java
+++ src/test/java/InitialMapTest.java
@@ -1,25 +1,40 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import se.oru.wildfire.Cell;
+import se.oru.wildfire.Coordinate;
+import se.oru.wildfire.InitialMap;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class InitialMapTest {
-    @BeforeEach
-    void setUp() {
-    }
 
-    @AfterEach
-    void tearDown() {
+    static InitialMap map;
+    static Cell[][] cells;
+
+    @BeforeAll
+    static void setUp() {
+        cells = new Cell[3][3];
+        for (int i=0;i<3;i++){
+            for (int j=0;j<3;j++){
+                cells[i][j] = new Cell((int) (Math.random() * 100));
+            }
+        }
+        map = new InitialMap(cells);
     }
 
     @Test
     void getSize() {
-        fail();
+        Coordinate coordinate = map.getSize();
+        assertEquals(coordinate.x(), 3);
+        assertEquals(coordinate.y(), 3);
     }
 
     @Test
     void getCell() {
-        fail();
+        for (int i=0;i<3;i++){
+            for (int j=0;j<3;j++){
+                Cell cell = map.getCell(i, j);
+                assertEquals(cell, cells[i][j]);
+            }
+        }
     }
 }
