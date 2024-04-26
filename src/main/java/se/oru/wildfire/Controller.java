13a2055e8b91462baac10540a717404709ee0cfd
David Falk <dafa2100@student.miun.se>
2024-04-26 12:06:12 +0200

    Instantiate Controller

--- src/main/java/se/oru/wildfire/Controller.java
+++ src/main/java/se/oru/wildfire/Controller.java
@@ -8,14 +8,15 @@ import java.awt.event.ActionListener;
 
 public class Controller implements ActionListener {
 
-    final View view = new View();
+    final View view ;
     final Model model = new Model();
     final Calculator calculator = new Calculator();
     final FrameStatistics frameStatistics = new FrameStatistics();
 
     Timer timer = new Timer(1000, this);
 
-    public Controller(){
+    public Controller(View view){
+        this.view = view;
         model.registerListener(view);
         model.registerListener(frameStatistics);
         model.registerListener(calculator);
