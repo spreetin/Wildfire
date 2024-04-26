70d084f37a874a643d322ca6a9e1104736d80686
vikander86 <bager86@gmail.com>
2024-04-26 11:44:05 +0200

    First Commit

--- src/main/java/se/oru/wildfire/View.java
+++ src/main/java/se/oru/wildfire/View.java
@@ -1,15 +1,71 @@
 package se.oru.wildfire;
 
 import javafx.scene.layout.GridPane;
-import se.oru.wildfire.Coordinate;
-import se.oru.wildfire.Observer;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+import java.awt.*;
+import java.util.Objects;
 
 public class View extends GridPane implements Observer {
+    private Rectangle[][] cells;
+
+    public Rectangle[][] getCells(){
+        return cells;
+    }
+
     @Override
     public void newUpdate(Notifier o) {
         // Cast o to Model class
         Coordinate[] updatedCells = o.updatedCells();
         // TODO: Implement
+        for(Coordinate coordinates : updatedCells){
+            drawCell(o.retrieveCell(coordinates), coordinates);
+        }
     }
 
+    public void redrawMap(){
+
+    }
+
+    public void setInitialMap(InitialMap initialMap){
+        Coordinate size = initialMap.getSize();
+        cells = new Rectangle[size.x()][size.y()];
+        for (int i = 0; i < size.x(); i++) {
+            for (int j = 0; j < size.y(); j++) {
+                Rectangle rect = new Rectangle(10, 10);
+                Cell cell = initialMap.getCell(i, j);
+                Coordinate coordinate = new Coordinate(i,j);
+                drawCell(cell, coordinate);
+                rect.setStroke(Color.BLACK);
+                int finalI = i;
+                int finalJ = j;
+                rect.setOnMouseClicked(_ -> handleCellClick(finalI, finalJ));
+                this.add(rect, j, i);
+                cells[i][j] = rect;
+            }
+        }
+    }
+
+    public void drawCell(Cell cell, Coordinate coordinates){
+        if(cell.isBurning()){
+            cells[coordinates.x()][coordinates.y()].setFill(Color.DARKORANGE);
+        } else {
+            if (cell.getGroundType() == Cell.GroundType.Trees) {
+                cells[coordinates.x()][coordinates.y()].setFill(Color.DARKGREEN);
+            } else if (cell.getGroundType() == Cell.GroundType.Water) {
+                cells[coordinates.x()][coordinates.y()].setFill(Color.DARKBLUE);
+            } else if (cell.getGroundType() == Cell.GroundType.Stone) {
+                cells[coordinates.x()][coordinates.y()].setFill(Color.DARKGREY);
+            }
+        }
+    }
+
+    private void handleCellClick(int i, int j) {
+        Rectangle rect = cells[i][j];
+        Color currentColor = (Color) rect.getFill();
+        // needs to get color from paint class
+        Color newColor = currentColor.equals(Color.WHITE) ? Color.DARKGREEN : Color.WHITE;
+        rect.setFill(newColor);
+    }
 }
--- src/test/java/se/oru/wildfire/ViewTest.java
+++ src/test/java/se/oru/wildfire/ViewTest.java
@@ -1,12 +1,43 @@
 package se.oru.wildfire;
 
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ViewTest {
+    private View view;
+    private InitialMap initialMap;
+
+    @BeforeEach
+    public void setUp() {
+        view = new View();
+        Cell[][] cells = new Cell[3][3];
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                cells[i][j] = new Cell();
+                cells[i][j].setGroundType(Cell.GroundType.Trees); //set all cells to groundtype trees.
+            }
+        }
+        cells[2][2].setGroundType(Cell.GroundType.Stone); // set 1 cell to goruntype Stone.
+        initialMap = new InitialMap(cells);
+    }
+
     @Test
-    void newUpdate() {
+    public void setInitialMapTest() {
+        view.setInitialMap(initialMap);
+        Rectangle rect = view.getCells()[0][0];
+        assertNotNull(rect); // make sure not null
+        assertEquals(Color.DARKGREEN, rect.getFill()); // make sure cell[0][0] is Green
+        rect = view.getCells()[2][2]; // update to a new cell
+        assertEquals(Color.DARKGREY, rect.getFill()); // make sure cell[0][0] is Green
+    }
+
+    @Test
+    void newUpdateTest() {
         fail();
     }
+
 }
