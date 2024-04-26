cd8d3fbe7eae69fbe122fb80984805d5b5e8c440
David Falk <dafa2100@student.miun.se>
2024-04-26 17:28:45 +0200

    Fixed tests a bit

--- src/test/java/se/oru/wildfire/ViewTest.java
+++ src/test/java/se/oru/wildfire/ViewTest.java
@@ -2,18 +2,19 @@ package se.oru.wildfire;
 
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ViewTest {
-    private View view;
-    private InitialMap initialMap;
 
-    @BeforeEach
-    public void setUp() {
-        view = new View();
+    @Test
+    @Order(1)
+    public void setInitialMap() {
+        View view = new View();
         Cell[][] cells = new Cell[3][3];
         for (int i = 0; i < cells.length; i++) {
             for (int j = 0; j < cells[i].length; j++) {
@@ -22,11 +23,7 @@ public class ViewTest {
             }
         }
         cells[2][2].setGroundType(Cell.GroundType.Stone); // set 1 cell to groundtype Stone.
-        initialMap = new InitialMap(cells);
-    }
-
-    @Test
-    public void setInitialMapTest() {
+        InitialMap initialMap = new InitialMap(cells);
         view.setInitialMap(initialMap);
         Rectangle rect = view.getCells()[0][0];
         assertNotNull(rect); // make sure not null
@@ -36,8 +33,34 @@ public class ViewTest {
     }
 
     @Test
-    void newUpdateTest() {
+    @Order(3)
+    void newUpdate() {
         fail();
     }
 
+    @Test
+    @Order(2)
+    void drawCell(){
+        View view = new View();
+        Cell[][] cells = new Cell[3][3];
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                cells[i][j] = new Cell();
+                cells[i][j].setGroundType(Cell.GroundType.Trees); //set all cells to groundtype trees.
+            }
+        }
+        cells[2][2].setGroundType(Cell.GroundType.Stone); // set 1 cell to groundtype Stone.
+        InitialMap initialMap = new InitialMap(cells);
+        view.setInitialMap(initialMap);
+        Cell cell = new Cell();
+        cell.setGroundType(Cell.GroundType.Water);
+        view.drawCell(cell, new Coordinate(2, 2));
+        assertEquals(Color.DARKBLUE, view.getCells()[2][2].getFill());
+    }
+
+    @Test
+    @Order(4)
+    void handleCellClick(){
+        fail();
+    }
 }
