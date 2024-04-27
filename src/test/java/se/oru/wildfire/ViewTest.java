edb2ac1ed521ca0af8e3650c6fbc8ef389f9c617
David Falk <dafa2100@student.miun.se>
2024-04-27 10:35:04 +0200

    Fixed test in ViewTest and added some function declarations for painting

--- src/test/java/se/oru/wildfire/ViewTest.java
+++ src/test/java/se/oru/wildfire/ViewTest.java
@@ -7,12 +7,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ViewTest {
 
     @Test
-    @Order(1)
     public void setInitialMap() {
         View view = new View();
         Cell[][] cells = new Cell[3][3];
@@ -33,13 +35,29 @@ public class ViewTest {
     }
 
     @Test
-    @Order(3)
     void newUpdate() {
-        fail();
+        View view = new View();
+        Cell[][] cells = new Cell[3][3];
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                cells[i][j] = new Cell();
+                cells[i][j].setGroundType(Cell.GroundType.Trees); //set all cells to groundtype trees.
+            }
+        }// set 1 cell to groundtype Stone.
+        InitialMap initialMap = new InitialMap(cells);
+        view.setInitialMap(initialMap);
+        NotifierTestClass notifierTest = new NotifierTestClass();
+        Map<Coordinate, Cell> map = new HashMap<>();
+        Cell cell = new Cell(50);
+        cell.setGroundType(Cell.GroundType.Water);
+        map.put(new Coordinate(2, 2), cell);
+        notifierTest.setMap(map);
+        view.newUpdate(notifierTest);
+        assertEquals(Color.DARKGREEN, view.getCells()[0][2].getFill());
+        assertEquals(Color.DARKBLUE, view.getCells()[2][2].getFill());
     }
 
     @Test
-    @Order(2)
     void drawCell(){
         View view = new View();
         Cell[][] cells = new Cell[3][3];
@@ -59,8 +77,12 @@ public class ViewTest {
     }
 
     @Test
-    @Order(4)
     void handleCellClick(){
+        fail();
+    }
+
+    @Test
+    void redrawMap() {
         fail();
     }
 }
